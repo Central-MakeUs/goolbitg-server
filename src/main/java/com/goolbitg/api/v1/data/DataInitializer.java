52e94f59f6b9134700da239323537bcf3d2421e1
wingyou <winwing.you@gmail.com>
2025-03-04 23:46:37 +0900

    feat(admin): 루트 계정 설정 변경

--- src/main/java/com/goolbitg/api/v1/data/DataInitializer.java
+++ src/main/java/com/goolbitg/api/v1/data/DataInitializer.java
@@ -2,12 +2,13 @@ package com.goolbitg.api.v1.data;
 
 import java.util.Set;
 
-import com.goolbitg.api.v1.security.AdminUserDetailsService;
-import com.goolbitg.api.v1.security.RoleType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import com.goolbitg.api.v1.security.AdminUserDetailsService;
+import com.goolbitg.api.v1.security.RoleType;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,11 +19,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@Profile({ "local" })
+@Profile({ "local", "dev", "prod" })
 public class DataInitializer implements CommandLineRunner {
 
     private final AdminUserDetailsService adminUserDetailsService;
-
+    
+    @Value("${admin.username}")
+    private String adminUsername;
     @Value("${admin.password}")
     private String adminPassword;
 
@@ -31,7 +34,7 @@ public class DataInitializer implements CommandLineRunner {
         log.info("Load Initial Data");
 
         adminUserDetailsService.registerAdminUser(
-            "goolbitg@gmail.com",
+            adminUsername,
             adminPassword,
             Set.of(RoleType.ROLE_ADMIN, RoleType.ROLE_MANAGER, RoleType.ROLE_DEVELOPER)
         );
