580ac9941b1da65444eef0a3b7bc3b6ce6ad2889
wingyou <winwing.you@gmail.com>
2026-01-25 21:54:44 +0900

    Improve deploy script

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 ./gradlew build -x test
+
+if [ $? -ne 0 ]; then
+  echo "Failed to build"
+  exit 1
+fi
+
 jar=$(find build/libs/ -name "*SNAPSHOT.jar")
 
+if [ -z "$jar" ]; then
+  echo "Cannot find artifect"
+  exit 1
+fi
+
 scp $jar gdev:~/apps/goolbitg.jar
+
+if [ $? -ne 0 ]; then
+  echo "Failed deploy artifect"
+  exit 1
+fi
+
 ssh gdev "source ~/.bashrc && /home/dogchew/scripts/restart.sh"
--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 ./gradlew build -x test
+
+if [ $? -ne 0 ]; then
+  echo "Failed to build"
+  exit 1
+fi
+
 jar=$(find build/libs/ -name "*SNAPSHOT.jar")
 
+if [ -z "$jar" ]; then
+  echo "Cannot find artifect"
+  exit 1
+fi
+
 scp $jar gprod:~/apps/goolbitg.jar
+
+if [ $? -ne 0 ]; then
+  echo "Failed deploy artifect"
+  exit 1
+fi
+
 ssh gprod "source ~/.bashrc && /home/mk2/scripts/restart.sh"
